c30c150cb064674934d2f46a256baedd6066fcb9
EAST/野村賢吾 <nomura@est.co.jp>
2025-06-23 10:35:57 +0900

    add Any Models All Create

---
+++ Ensyu_E-PAN/Models/Attendance/DayShift.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ensyu_E_PAN.Models.Attendance
+{
+    public class DayShift
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [ForeignKey("AllShift")]
+        public int All_Shift_Id { get; set; }
+
+        [Required]
+        public int Cost { get; set; }
+
+        [Required]
+        public TimeSpan Sum_WorkTime { get; set; }
+
+        [Required]
+        public int Sum_TotalCost { get; set; }
+
+        // ナビゲーションプロパティ（必要に応じて）
+        public AllShift AllShift { get; set; }
+
+    }
+}
---
+++ Ensyu_E-PAN/Models/Order/OrderItemList.cs
@@ -0,0 +1,29 @@
+﻿using Ensyu_E_PAN.Models.Masters;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ensyu_E_PAN.Models.Order
+{
+    public class OrderItemList
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("PurchaseOrder")]
+        public int P_Order_List_Id { get; set; }
+
+        [Required]
+        [ForeignKey("Item")]
+        public int Item_Cd { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        // ナビゲーションプロパティ
+        public PurchaseOrder PurchaseOrder { get; set; }
+        public Item Item { get; set; }
+
+    }
+}

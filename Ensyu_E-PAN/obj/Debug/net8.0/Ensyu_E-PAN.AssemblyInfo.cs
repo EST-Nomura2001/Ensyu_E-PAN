b9f5f86f74a44c4fd49dc64cb9d400dbb540586e
EAST/野村賢吾 <nomura@est.co.jp>
2025-06-24 11:33:47 +0900

    Edit Controller and add insert demo date

--- Ensyu_E-PAN/obj/Debug/net8.0/Ensyu_E-PAN.AssemblyInfo.cs
+++ Ensyu_E-PAN/obj/Debug/net8.0/Ensyu_E-PAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ensyu_E-PAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca7c57a41db8b9f7af847e026f75eadca89d95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64d6679085579a36a0332a2bb397031441d84c0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ensyu_E-PAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ensyu_E-PAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

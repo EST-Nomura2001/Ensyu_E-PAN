3aa0eebb2a13526aabecf4c70f0b5f19cc9572fc
EAST/野村賢吾 <nomura@est.co.jp>
2025-06-25 16:22:56 +0900

    add any put for attendance

---
+++ Ensyu_E-PAN/DTOs/UpdateAttendance/UpdatePlannedWorkTimeRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Ensyu_E_PAN.DTOs.UpdateAttendance
+{
+    public class UpdatePlannedWorkTimeRequest
+    {
+        public DateTime TargetDate { get; set; }
+        public DateTime? P_Start_WorkTime { get; set; }
+        public DateTime? P_End_WorkTime { get; set; }
+
+    }
+}
---
+++ Ensyu_E-PAN/DTOs/UpdateAttendance/UpdateUserShiftRequest.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Ensyu_E_PAN.DTOs.UpDateAttendance
+{
+	public class UpdateUserShiftRequest
+	{
+		public DateTime TargetDate { get; set; } // どの日付の勤務予定を対象にするか
+		public DateTime? U_Start_WorkTime { get; set; }
+		public DateTime? U_End_WorkTime { get; set; }
+		public bool U_Confirm_Flg { get; set; }
+	}
+}

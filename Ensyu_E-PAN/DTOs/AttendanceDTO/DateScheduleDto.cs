1f42bd6e2fb20d1950eeea3e00c5f59b3f7b87c8
EAST/野村賢吾 <nomura@est.co.jp>
2025-06-27 11:34:01 +0900

    edit attendance

--- Ensyu_E-PAN/DTOs/AttendanceDTO/DateScheduleDto.cs
+++ Ensyu_E-PAN/DTOs/AttendanceDTO/DateScheduleDto.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public DateTime Today { get; set; }
-
         public DateTime? P_Start_WorkTime { get; set; }
         public DateTime? P_End_WorkTime { get; set; }
         public DateTime? U_Start_WorkTime { get; set; }
@@ -13,20 +12,16 @@
         public DateTime? End_WorkTime { get; set; }
         public DateTime? Start_BreakTime { get; set; }
         public DateTime? End_BreakTime { get; set; }
-
         public TimeSpan? T_WorkTime_D { get; set; }
         public TimeSpan? T_WorkTime_N { get; set; }
         public TimeSpan? T_WorkTime_All { get; set; }
-
         public int? D_DayPrice { get; set; }
         public int? N_DayPrice { get; set; }
         public int? T_DayPrice { get; set; }
-
-        // 参照情報
         public string UserName { get; set; }
+        public int WorkRollId { get; set; }
         public string WorkRollName { get; set; }
-        public DateTime? DayShiftDate { get; set; }
-
+        public int DayShiftId { get; set; }
         public bool? U_Confirm_Flg { get; set; }
     }
 }
